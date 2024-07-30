f32520e6c184876b77eeb82db2ea37d0cfd4b904
stkch0832 <stkch0832@gmail.com>
2024-07-30 09:50:16 +0900

    Add: account delete and change email and password

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-from django.views.generic.base import TemplateView
 from accounts.views import user_views, profile_views
 
 app_name = 'accounts'
@@ -7,7 +6,10 @@ app_name = 'accounts'
 urlpatterns = [
     path('signup/', user_views.SignupView.as_view(), name="accounts_signup"),
     path('login/', user_views.LoginView.as_view(), name="accounts_login"),
+    path('delete/', user_views.AccountDeleteView.as_view(), name="accounts_delete"),
 
     path('profile/', profile_views.ProfileDetailView.as_view(), name='profile_detail'),
     path('profile/edit/', profile_views.ProfileUpdateView.as_view(), name='profile_edit'),
+
+
 ]
--- accounts/views/user_views.py
+++ accounts/views/user_views.py
@@ -6,7 +6,13 @@ from allauth.account.views import (
     SignupView,
     LoginView,
     )
+from django.urls import reverse_lazy
+from django.contrib.auth import get_user_model
+from django.views.generic.edit import DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
 
+User = get_user_model()
 
 class SignupView(SignupView):
     template_name = 'account/signup.html'
@@ -16,3 +22,13 @@ class SignupView(SignupView):
 class LoginView(LoginView):
     template_name = 'account/login.html'
     form_class = CustomLoginForm
+
+
+class AccountDeleteView(LoginRequiredMixin, SuccessMessageMixin, DeleteView):
+    model = User
+    template_name = 'account/delete.html'
+    success_url = reverse_lazy('app:post_list')
+    success_message = "アカウントを削除しました。ご利用いただきありがとうございました。"
+
+    def get_object(self):
+        return self.request.user
